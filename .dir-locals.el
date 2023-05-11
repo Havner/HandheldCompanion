f1e053e44bfee04d3c47286fcd8da3a551bf149a
Lukasz Pawelczyk <havner@gmail.com>
2023-05-11 17:57:59 +0200

    Add dir-locals

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((projectile-project-compilation-cmd . "dotnet build ControllerService.sln --no-restore /p:WarningLevel=0 --configuration Release")
+         (indent-tabs-mode . nil))))
