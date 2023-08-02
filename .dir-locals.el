937425ee731817699079e415f4f2c2cc334359f0
Lukasz Pawelczyk <havner@gmail.com>
2023-08-02 03:27:00 +0200

    Fix solution name in dir locals

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,5 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil . ((projectile-project-compilation-cmd . "dotnet build ControllerService.sln --no-restore /p:WarningLevel=0 --configuration Release")
+((nil . ((projectile-project-compilation-cmd . "dotnet build ControllerCompanion.sln --no-restore /p:WarningLevel=0 --configuration Release")
          (indent-tabs-mode . nil))))
